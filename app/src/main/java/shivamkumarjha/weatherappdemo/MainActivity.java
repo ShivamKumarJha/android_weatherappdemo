0b178f25039a6a8385af2a91fdc4d5008073c7c3
ShivamKumarJha <jha.shivam3@gmail.com>
2017-05-30 15:24:10 +0530

    improvements

--- app/src/main/java/shivamkumarjha/weatherappdemo/MainActivity.java
+++ app/src/main/java/shivamkumarjha/weatherappdemo/MainActivity.java
@@ -1,88 +1,234 @@
 package shivamkumarjha.weatherappdemo;
-
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
-    JSONObject data = null;
-    TextView t;
+    EditText editTextCityName;
+    Button btnByCityName;
+    TextView textViewResult, textViewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t = (TextView) findViewById(R.id.res);
-        getJSON("Hyderabad,IN");
+        editTextCityName = (EditText)findViewById(R.id.cityname);
+        btnByCityName = (Button)findViewById(R.id.bycityname);
+        textViewResult = (TextView)findViewById(R.id.result);
+        textViewInfo = (TextView)findViewById(R.id.info);
+
+        btnByCityName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new OpenWeatherMapTask(
+                        editTextCityName.getText().toString(),
+                        textViewResult).execute();
+            }
+        });
     }
 
-    public void getJSON(final String city) {
+    private class OpenWeatherMapTask extends AsyncTask<Void, Void, String> {
 
-        new AsyncTask<Void, Void, Void>() {
+        String cityName;
+        TextView tvResult;
 
+        String dummyAppid = "302314f40333e0f47f0f9e9cb1e5a073";
+        String queryWeather = "http://api.openweathermap.org/data/2.5/weather?q=";
+        String queryDummyKey = "&appid=" + dummyAppid;
 
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
+        OpenWeatherMapTask(String cityName, TextView tvResult){
+            this.cityName = cityName;
+            this.tvResult = tvResult;
+        }
 
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = "";
+            String queryReturn;
+
+            String query = null;
+            try {
+                query = queryWeather + URLEncoder.encode(cityName, "UTF-8") + queryDummyKey;
+                queryReturn = sendQuery(query);
+                result += ParseJSON(queryReturn);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                queryReturn = e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                queryReturn = e.getMessage();
             }
 
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=302314f40333e0f47f0f9e9cb1e5a073");
 
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            final String finalQueryReturn = query + "\n\n" + queryReturn;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textViewInfo.setText(finalQueryReturn);
+                }
+            });
 
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-                    StringBuffer json = new StringBuffer(1024);
-                    String tmp = "";
+            return result;
+        }
 
-                    while((tmp = reader.readLine()) != null)
-                        json.append(tmp).append("\n");
-                    reader.close();
+        @Override
+        protected void onPostExecute(String s) {
+            tvResult.setText(s);
+        }
 
-                    data = new JSONObject(json.toString());
+        private String sendQuery(String query) throws IOException {
+            String result = "";
 
-                    if(data.getInt("cod") != 200) {
-                        System.out.println("Cancelled");
-                        return null;
+            URL searchURL = new URL(query);
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection)searchURL.openConnection();
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(
+                        inputStreamReader,
+                        8192);
+
+                String line = null;
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+
+                bufferedReader.close();
+            }
+
+            return result;
+        }
+
+        private String ParseJSON(String json){
+            String jsonResult = "";
+
+            try {
+                JSONObject JsonObject = new JSONObject(json);
+                String cod = jsonHelperGetString(JsonObject, "cod");
+
+                if(cod != null){
+                    if(cod.equals("200")){
+
+                        jsonResult += jsonHelperGetString(JsonObject, "name") + "\n";
+                        JSONObject sys = jsonHelperGetJSONObject(JsonObject, "sys");
+                        if(sys != null){
+                            jsonResult += jsonHelperGetString(sys, "country") + "\n";
+                        }
+                        jsonResult += "\n";
+
+                        JSONObject coord = jsonHelperGetJSONObject(JsonObject, "coord");
+                        if(coord != null){
+                            String lon = jsonHelperGetString(coord, "lon");
+                            String lat = jsonHelperGetString(coord, "lat");
+                            jsonResult += "lon: " + lon + "\n";
+                            jsonResult += "lat: " + lat + "\n";
+                        }
+                        jsonResult += "\n";
+
+                        JSONArray weather = jsonHelperGetJSONArray(JsonObject, "weather");
+                        if(weather != null){
+                            for(int i=0; i<weather.length(); i++){
+                                JSONObject thisWeather = weather.getJSONObject(i);
+                                jsonResult += "weather " + i + ":\n";
+                                jsonResult += "id: " + jsonHelperGetString(thisWeather, "id") + "\n";
+                                jsonResult += jsonHelperGetString(thisWeather, "main") + "\n";
+                                jsonResult += jsonHelperGetString(thisWeather, "description") + "\n";
+                                jsonResult += "\n";
+                            }
+                        }
+
+                        JSONObject main = jsonHelperGetJSONObject(JsonObject, "main");
+                        if(main != null){
+                            jsonResult += "temp: " + jsonHelperGetString(main, "temp") + "\n";
+                            jsonResult += "pressure: " + jsonHelperGetString(main, "pressure") + "\n";
+                            jsonResult += "humidity: " + jsonHelperGetString(main, "humidity") + "\n";
+                            jsonResult += "temp_min: " + jsonHelperGetString(main, "temp_min") + "\n";
+                            jsonResult += "temp_max: " + jsonHelperGetString(main, "temp_max") + "\n";
+                            jsonResult += "sea_level: " + jsonHelperGetString(main, "sea_level") + "\n";
+                            jsonResult += "grnd_level: " + jsonHelperGetString(main, "grnd_level") + "\n";
+                            jsonResult += "\n";
+                        }
+
+                        jsonResult += "visibility: " + jsonHelperGetString(JsonObject, "visibility") + "\n";
+                        jsonResult += "\n";
+
+                        JSONObject wind = jsonHelperGetJSONObject(JsonObject, "wind");
+                        if(wind != null){
+                            jsonResult += "wind:\n";
+                            jsonResult += "speed: " + jsonHelperGetString(wind, "speed") + "\n";
+                            jsonResult += "deg: " + jsonHelperGetString(wind, "deg") + "\n";
+                            jsonResult += "\n";
+                        }
+
+                        //...incompleted
+
+                    }else if(cod.equals("404")){
+                        String message = jsonHelperGetString(JsonObject, "message");
+                        jsonResult += "cod 404: " + message;
                     }
-
-
-                } catch (Exception e) {
-
-                    System.out.println("Exception "+ e.getMessage());
-                    return null;
+                }else{
+                    jsonResult += "cod == null\n";
                 }
 
-                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                jsonResult += e.getMessage();
             }
 
-            @Override
-            protected void onPostExecute(Void Void) {
-                if(data!=null){
-                    Log.d("my weather received",data.toString());
-                    t.setText(data.toString());
-                }
+            return jsonResult;
+        }
 
+        private String jsonHelperGetString(JSONObject obj, String k){
+            String v = null;
+            try {
+                v = obj.getString(k);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        }.execute();
 
+            return v;
+        }
+
+        private JSONObject jsonHelperGetJSONObject(JSONObject obj, String k){
+            JSONObject o = null;
+
+            try {
+                o = obj.getJSONObject(k);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return o;
+        }
+
+        private JSONArray jsonHelperGetJSONArray(JSONObject obj, String k){
+            JSONArray a = null;
+
+            try {
+                a = obj.getJSONArray(k);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return a;
+        }
     }
 }
