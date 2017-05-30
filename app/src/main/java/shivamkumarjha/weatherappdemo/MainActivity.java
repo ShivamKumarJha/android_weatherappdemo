5f8f8fd5736e4533dc687e13355b85a15d03807c
ShivamKumarJha <jha.shivam3@gmail.com>
2017-05-30 15:10:16 +0530

    Initial source code

---
+++ app/src/main/java/shivamkumarjha/weatherappdemo/MainActivity.java
@@ -0,0 +1,88 @@
+package shivamkumarjha.weatherappdemo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    JSONObject data = null;
+    TextView t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t = (TextView) findViewById(R.id.res);
+        getJSON("Hyderabad,IN");
+    }
+
+    public void getJSON(final String city) {
+
+        new AsyncTask<Void, Void, Void>() {
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=302314f40333e0f47f0f9e9cb1e5a073");
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuffer json = new StringBuffer(1024);
+                    String tmp = "";
+
+                    while((tmp = reader.readLine()) != null)
+                        json.append(tmp).append("\n");
+                    reader.close();
+
+                    data = new JSONObject(json.toString());
+
+                    if(data.getInt("cod") != 200) {
+                        System.out.println("Cancelled");
+                        return null;
+                    }
+
+
+                } catch (Exception e) {
+
+                    System.out.println("Exception "+ e.getMessage());
+                    return null;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void Void) {
+                if(data!=null){
+                    Log.d("my weather received",data.toString());
+                    t.setText(data.toString());
+                }
+
+            }
+        }.execute();
+
+    }
+}
